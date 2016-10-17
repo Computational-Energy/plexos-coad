f4d48209dc51b0493064c2a585d986fb3c39968f
Harry Sorensen <harry.sorensen@nrel.gov>
2016-10-17 07:30:21 -0600

    Fix setup to install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(name='coad',
+      version='1.0',
+      description='Class-Object-Attribute Data manipution for Plexos',
+      author='Harry Sorensen',
+      author_email='harry.sorensen@nrel.gov',
+      packages=['coad'],
+      ext_modules=cythonize("coad/compress_interval.pyx")
+    )
