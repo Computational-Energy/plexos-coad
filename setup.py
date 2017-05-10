304576f8d0800e75c7eac94cee277c6858b3d121
Harry Sorensen <harry.sorensen@nrel.gov>
2017-05-10 07:44:13 -0600

    Version number to reflect new funtionality

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(name='coad',
-      version='1.1',
+      version='2.0',
       description='Class-Object-Attribute Data manipution for Plexos',
       author='Harry Sorensen',
       author_email='harry.sorensen@nrel.gov',
